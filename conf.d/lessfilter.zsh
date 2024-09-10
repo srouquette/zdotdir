dda33e70315f7c611e3c519b66a51b579590aebd
Syl ⏚ <srouquette@gmail.com>
2024-09-10 23:21:20 +0200

    [less] use chafa first, with view size

--- conf.d/lessfilter.zsh
+++ conf.d/lessfilter.zsh
@@ -4,5 +4,7 @@
 
 if [ ! -f ${HOME}/.lessfilter ]; then
   ln -s ${ZDOTDIR}/home/.lessfilter ${HOME}/.lessfilter
-  chmod +x ${HOME}/.lessfilter
 fi
+
+chmod +x ${ZDOTDIR}/home/.lessfilter
+chmod +x ${HOME}/.lessfilter
