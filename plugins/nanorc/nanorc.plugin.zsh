6da05d653ff19d7a548c845f4b07c0bffb5a8972
Syl ⏚ <srouquette@gmail.com>
2024-09-29 12:59:44 +0200

    [nano] add additional paths

--- plugins/nanorc/nanorc.plugin.zsh
+++ plugins/nanorc/nanorc.plugin.zsh
@@ -4,8 +4,12 @@
 
 function update_nanorc {
   paths=(
+    "/usr/share/nano"
+    "/usr/share/nano/extra"
+    "/usr/share/local/nano"
     '/usr/share/nano-syntax-highlighting'
-    "$HOME/.nanorc"
+    "$HOME/.nano"
+    "$HOME/.local/share/nano"
   )
   for p in $paths; do
     [ -d $p ] || continue
